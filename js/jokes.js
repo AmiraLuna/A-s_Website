a507fd5365150cc41731e9f167afaa46e0fcd01f
Amirah Luna <112425108+AmiraLuna@users.noreply.github.com>
2023-03-20 17:05:40 +0000

    Mon, Mar 20, 2023, 10:05 AM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -4,9 +4,9 @@ const jokeBtn = document.getElementById('jokeBtn')
 jokeBtn.addEventListener('click', generateJoke)
 
 async function generateJoke(){
-    const res = await fetch('https://icanhazdadjoke.com'), {
-        headers: {Accept: 'application/json'}
-    }
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json',},
+    })
 
     const data = await res.json()
     jokeEl.innerHTML = data.joke
