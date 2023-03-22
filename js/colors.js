6feada95fa1b734d74590c5c36bf87d60b1a0566
Amirah Luna <112425108+AmiraLuna@users.noreply.github.com>
2023-03-22 17:11:46 +0000

    Wed, Mar 22, 2023, 10:11 AM -07:00

--- js/colors.js
+++ js/colors.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('conatiner')
+const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 const SQUARES = 500
 
@@ -6,11 +6,12 @@ for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
     container.appendChild(square)
-
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
 
 }
 
-funtion removeColor(element) {
+function removeColor(element) {
     element.style.background = '#1d1d1d'
 }
 
